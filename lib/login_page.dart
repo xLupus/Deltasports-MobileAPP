b2e794af4493f29543d16d61b6a0020bfc7a060e
Raffaviana <jackson.vi.ana@hotmail.com>
2023-03-24 00:48:17 -0300

    tela de login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -15,96 +14,133 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              Icon(
-                Icons.phone_android,
-                size: 100,
-              ),
-              SizedBox(height: 75),
-        //BemVindo
-        Text(
-          'Bem-vindo(a)',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-            ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          'Faça o login ou cadastre-se para continuar',
-          style: TextStyle(
-            fontSize: 16,
-            ),
-        ),
-        SizedBox(height: 18),
+          child: Column(children: [
+            Image.asset('imagens/logo.png',
+            width: 120,
+            height: 100),
 
-        //Email
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Email',
-            ),
-          ),
-        ),
-      ),
-    ),
-    SizedBox(height: 18),
-    //Senha
-    Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Senha',
-            ),
-          ),
-        ),
-      ),
-    ),
-    SizedBox(height: 18),
-    //Btn entrar
-    Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Container(
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Center(
-          child: Text(
-            'Entrar',
-            style:TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
+            SizedBox(height: 75),
+
+            //BemVindo
+            Container(
+              child: Align(
+                alignment: Alignment(-0.75,0.0),
+                child: Text(
+                  'Bem-vindo(a)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
-  ]
-          ),
+
+            SizedBox(height: 10),
+
+            Container(
+              child: Align(
+                alignment: Alignment(-0.4,0.0),
+                child: Text(
+                  'Faça o login ou cadastre-se para continuar',
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 18),
+
+            //Email 0xFF1C8394
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 18),
+
+            //Senha
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Senha',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 80),
+
+            //Btn entrar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0XFFa52502),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Color(0xFF154b52)),
+                ),
+                child: Center(
+                  child: Text(
+                    'Voltar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
