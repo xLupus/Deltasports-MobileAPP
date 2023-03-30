7740624691ccd7e8151e0afd6b3a2533ac919c8e
LuccaB <lbordin.design@gmail.com>
2023-03-30 16:07:07 -0300

    Subindo progresso na pagina de Produtos

--- lib/produtos.dart
+++ lib/produtos.dart
@@ -1,5 +1,7 @@
 import 'package:deltasports_app/utilis/global_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:deltasports_app/utilis/footer.dart';
+
 
 class ProdutosPage extends StatefulWidget {
   const ProdutosPage({Key? key}) : super(key: key);
@@ -16,85 +18,174 @@ class _ProdutosPageState extends State<ProdutosPage> {
       body: SafeArea(
         child: Center(
           child: Column(children: [
-            SizedBox(height: 150),
-            Image.network('https://i.imgur.com/aSEadiB.png'),
-            SizedBox(height: 205),
+            SizedBox(height: 20),
 
-            //Btn Login
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.of(context).pushReplacementNamed(
-                    '/Categorias'
-                  )
-                },
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: GlobalColors.red,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow:  [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 1,
-                      blurRadius: 1,
-                      offset: Offset(0, 5), // changes position of shadow
-                    ),
-                  ],
+            //Logo E Search
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Image.network('https://i.imgur.com/ell1sHu.png'),
+                Icon(
+                  Icons.search,
+                  color: GlobalColors.red,
+                  size: 45.0,
+                ),
+              ],
+            ),
+
+
+            SizedBox(height: 50),
+            //Titulo "Produtos e ADD"
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  'Produtos',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
                   ),
-                  child: Center(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: GlobalColors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                ),
+                Icon(
+                  Icons.add,
+                  color: GlobalColors.red,
+                  size: 45.0,
+                ),
+              ],
+            ),
+
+
+            SizedBox(height: 10),
+            //Categorias
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                //Tênis
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 05.0),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.of(context).pushReplacementNamed(
+                        '/Registrar'
+                      )
+                    },
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20,10,20,10),
+                    decoration: BoxDecoration(
+                      color: GlobalColors.blue,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Tênis',
+                        style: TextStyle(
+                          color: GlobalColors.white,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
+                ),//Fim_Tenis
 
-            SizedBox(height: 40),
-
-            //Btn Registrar
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.of(context).pushReplacementNamed(
-                    '/Registrar'
-                  )
-                },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: GlobalColors.blue,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow:  [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 1,
-                      offset: Offset(0, 5), // changes position of shadow
+                //Roupas
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 05.0),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.of(context).pushReplacementNamed(
+                        '/Registrar'
+                      )
+                    },
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20,10,20,10),
+                    decoration: BoxDecoration(
+                      color: GlobalColors.blue,
+                      borderRadius: BorderRadius.circular(12),
+ 
                     ),
-                  ],
-                ),
-                child: Center(
-                  child: Text(
-                    'Registrar',
-                    style: TextStyle(
-                      color: GlobalColors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                    child: Center(
+                      child: Text(
+                        'Roupas',
+                        style: TextStyle(
+                          color: GlobalColors.white,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                ),//Fim Roupas
+
+                //Acessórios
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 05.0),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.of(context).pushReplacementNamed(
+                        '/Registrar'
+                      )
+                    },
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20,10,20,10),
+                    decoration: BoxDecoration(
+                      color: GlobalColors.blue,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Acessórios',
+                        style: TextStyle(
+                          color: GlobalColors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ),//Fim Acessórios
+
+                //Bolas
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 05.0),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.of(context).pushReplacementNamed(
+                        '/Registrar'
+                      )
+                    },
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20,10,20,10),
+                    decoration: BoxDecoration(
+                      color: GlobalColors.blue,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Bolas',
+                        style: TextStyle(
+                          color: GlobalColors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ),//Fim Bolas
+
+              //Adicionar restante da categoria
+              ],
             ),
-            ),
+
+
+//Footer
+
+
+  
+//Final Footer
+
+
+
           ]),
         ),
       ),
